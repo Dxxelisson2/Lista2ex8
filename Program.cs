31f27e8236fc4c51ea8daa2faa27bcd2f0adabbe
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-05-03 20:04:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_2_ex_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.WriteLine("Digite o lado A: ");
+            a = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Digite o lado B: ");
+            b = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Digite o lado C: ");
+            c = Convert.ToInt16(Console.ReadLine());
+
+            //dados ( a² + b² = c² ) 
+            if (a*a==b*b+c*c||b*b==a*a+c*c||c*c==a*a+b*b)
+            {
+                Console.WriteLine("É um triângulo Retangulo!");
+            }
+
+            else
+            {
+                Console.WriteLine("Não é um triângulo Retangulo!");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
